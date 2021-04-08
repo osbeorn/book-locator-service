8ca117ed51a94cbb31814324396b9758faaee892
Benjamin Kastelic <benjamin.kastelic@sunesis.si>
2021-04-08 22:29:33 +0200

    minor

--- models/src/main/resources/db/migration/V1_0_3__refactor_rack_code_selector.sql
+++ models/src/main/resources/db/migration/V1_0_3__refactor_rack_code_selector.sql
@@ -1,7 +1,7 @@
 ALTER TABLE floors ADD COLUMN rack_code_attribute_selector VARCHAR NULL;
 ALTER TABLE floors ADD COLUMN rack_code_value_selector VARCHAR NULL;
 
-UPDATE floors SET rack_code_attribute_selector = rack_code_identifier;
+UPDATE floors SET rack_code_attribute_selector = rack_code_attribute_selector;
 
 ALTER TABLE floors ALTER COLUMN rack_code_attribute_selector SET NOT NULL;
 
